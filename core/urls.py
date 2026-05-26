b86ee734434ad906e6e82dd625171f1ad9b33e81
Denys Baliuk <denuk14228@gmail.com>
2026-05-26 14:42:44 +0300

    feat: implement instructor list view and template

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import car_list
+from .views import car_list, instructor_list, student_list
 
 
 urlpatterns = [
     path('cars/', car_list, name='car-list'),
+    path('instructors/', instructor_list, name='instructor-list'),
+    path('students/', student_list, name='student-list'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from .models import Car
+from .models import Car, Instructor, Student
 
 
 def car_list(request):
     cars = Car.objects.all()
     return render(request, 'core/car_list.html', {'cars': cars})
+
+
+def instructor_list(request):
+    instructors = Instructor.objects.all()
+    return render(request, 'core/instructor_list.html', {'instructors': instructors})
+
+
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'core/student_list.html', {'students': students})
