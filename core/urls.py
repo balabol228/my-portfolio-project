66c1bf99229b8b19bf93f05dee7b0df0b4c5333f
Denys Baliuk <denuk14228@gmail.com>
2026-05-26 15:24:32 +0300

    feat: implement Bootstrap 5 design for home page and fix PEP8

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import car_list, instructor_list, student_list
+from .views import index, car_list, instructor_list, student_list
 
 
 urlpatterns = [
+    path('', index, name='index'),
     path('cars/', car_list, name='car-list'),
     path('instructors/', instructor_list, name='instructor-list'),
     path('students/', student_list, name='student-list'),
--- core/views.py
+++ core/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render
 from .models import Car, Instructor, Student
 
 
+def index(request):
+    num_cars = Car.objects.count()
+    num_instructors = Instructor.objects.count()
+    num_students = Student.objects.count()
+
+    context = {
+        'num_cars': num_cars,
+        'num_instructors': num_instructors,
+        'num_students': num_students,
+    }
+    return render(request, 'core/index.html', context=context)
+
+
 def car_list(request):
     cars = Car.objects.all()
     return render(request, 'core/car_list.html', {'cars': cars})
